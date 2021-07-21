2fb2cb9d289ac313462c774fbd36f46c79bc68cd
Kenny Bui <kbui3145@gmail.com>
2021-07-21 15:35:55 -0700

    First commmit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,123 @@
+package com.mycompany.app;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+import com.mycompany.app.models.Company;
+import com.mycompany.app.models.Employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @SuppressWarnings("unchecked")
+	public static void main( String[] args )
+    {
+    	Configuration configuration = new Configuration().configure();
+    	StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+    	
+    	SessionFactory sessionFactory = configuration.buildSessionFactory(ssrb.build());
+    	Session session = sessionFactory.openSession();
+    	Transaction transaction = session.beginTransaction();
+    	
+    	Employee employeeOne = new Employee();
+    	employeeOne.setId(101);
+    	employeeOne.setFirstName("James");
+    	employeeOne.setLastName("Bond");
+
+    	Employee employeeTwo = new Employee();
+    	employeeTwo.setId(101);
+    	employeeTwo.setFirstName("Kelly");
+    	employeeTwo.setLastName("Mars");
+
+    	Employee employeeThree= new Employee();
+    	employeeThree.setId(101);
+    	employeeThree.setFirstName("Kenny");
+    	employeeThree.setLastName("Bui");
+
+    	session.save(employeeOne);
+    	session.save(employeeTwo);
+    	session.save(employeeThree);
+    	
+    	ArrayList<Employee> list1 = new ArrayList<Employee>();
+    	list1.add(employeeOne);
+    	list1.add(employeeTwo);
+    	ArrayList<Employee> list2 = new ArrayList<Employee>();
+    	list1.add(employeeTwo);
+    	list1.add(employeeThree);
+    	ArrayList<Employee> list3 = new ArrayList<Employee>();
+    	list1.add(employeeOne);
+    	list1.add(employeeThree);
+
+    	Company companyOne = new Company();
+    	companyOne.setCompanyName("Google");
+    	companyOne.setType("Technology");
+    	companyOne.setEmployees(list1);
+
+    	Company companyTwo = new Company();
+    	companyTwo.setCompanyName("Whole Foods");
+    	companyTwo.setType("Market");
+    	companyTwo.setEmployees(list2);
+    	
+    	Company companyThree = new Company();
+    	companyThree.setCompanyName("Best Buy");
+    	companyThree.setType("Electronics");
+    	companyThree.setEmployees(list3);
+
+    	session.persist(companyOne);
+    	session.persist(companyTwo);
+    	session.persist(companyThree);
+
+    	transaction.commit();
+    	System.out.println("Successfully saved.");
+
+    	// Query - parameterized HQL and native SQL
+    	System.out.println("Regular Query");
+    	List<Company> list = session.createQuery("from Company where id > :companyId").setInteger("companyId", 1).list();
+    	Iterator<Company> itr = list.iterator();
+    	Iterator<Employee> itr2;
+    	
+    	while(itr.hasNext()) {
+    		Company company = itr.next();
+    		System.out.println("Company name: "+ company.getCompanyName());
+
+    		List<Employee> employees = company.getEmployees();
+    		itr2 = employees.iterator();
+    		while(itr2.hasNext()) {
+    			Employee employee = itr2.next();
+    			System.out.println("	Employee: "+employee);
+    		}
+
+    	}
+    	
+    	// Criteria - SELECT statement
+    	System.out.println("Criteria");
+    	list = session.createCriteria(Company.class).add(Restrictions.between("id", 1, 4))
+    			.addOrder(Order.desc("companyName")).list();
+    	itr = list.iterator();
+    	while(itr.hasNext()) {
+    		Company company = itr.next();
+    		System.out.println("Company name: "+ company.getCompanyName());
+    	}
+
+    	
+    	// Persist Company from DB to Java Object
+    	System.out.println("Saved to Java Object");
+    	Company comp = (Company) session.get(Company.class, 3);
+    	System.out.println(comp);
+
+    	session.close();
+    	sessionFactory.close();
+    }
+}
---
+++ src/main/java/com/mycompany/app/models/Company.java
@@ -0,0 +1,52 @@
+package com.mycompany.app.models;
+
+import java.util.List;
+
+public class Company {
+	private int id;
+	
+	private String companyName;
+	
+	private String type;
+	
+	private List<Employee> employees;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [id=" + id + ", companyName=" + companyName + ", type=" + type + ", employees=" + employees
+				+ "]";
+	}
+	
+}
